adef8dfba280721b40c7cee2f0ce1f1aa288c2d9
Oğuzhan Kalkuz <kalkuzoguzhann@gmail.com>
2023-03-11 18:22:26 +0300

    Provided events and implemented healing logic. Closes #3, closes #4

--- Runtime/Character Stats/CharacterStats.cs
+++ Runtime/Character Stats/CharacterStats.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace KalkuzSystems.Stats
 {
-  public abstract class BaseCharacterStats : MonoBehaviour
+  public class CharacterStats : MonoBehaviour
   {
     [SerializeField] private bool autoInitialize;
     [SerializeField] private CharacterStatsData baseStats;
+
+    [SerializeField] private UnityEvent onSpawn;
+    [SerializeField] private UnityEvent onDodge;
+    [SerializeField] private UnityEvent onDamaged;
+    [SerializeField] private UnityEvent onCriticallyDamaged;
+    [SerializeField] private UnityEvent onHeal;
+    [SerializeField] private UnityEvent onDeath;
 
     private StatModifiers statModifiers;
 
@@ -23,40 +31,65 @@ namespace KalkuzSystems.Stats
       CurrentStats = baseStats.Copy();
 
       IsDied = false;
+      onSpawn?.Invoke();
     }
 
     public bool ShouldDodge(float accuracy)
     {
-      return accuracy < Mathf.Clamp(Random.value, 0, 0.99f);
+      var isDodge = accuracy < Mathf.Clamp(Random.value, 0, 0.99f);
+      if (isDodge) onDodge?.Invoke();
+
+      return isDodge;
     }
 
     public void ApplyDamage(Damage damage, float critChance, float critMultiplier)
     {
       var isCrit = Mathf.Clamp(Random.value, 0, 0.99f) < critChance;
 
-      var resistanceAmount = CurrentStats.CharacterResistances.Find(i => i.ResistantTo == damage.DamageType).PercentAmount;
+      var resistanceAmount =
+        CurrentStats.CharacterResistances.Find(i => i.ResistantTo == damage.DamageType).PercentAmount;
 
       var netDamage = damage * (1f - resistanceAmount);
       if (isCrit) netDamage *= critMultiplier;
 
       var sampledDamage = netDamage.Sample();
-      ChangeHealth(-sampledDamage);
+      if (sampledDamage >= 0f)
+      {
+        if (isCrit)
+        {
+          onCriticallyDamaged?.Invoke();
+        }
+        else
+        {
+          onDamaged?.Invoke();
+        }
+
+        ChangeHealth(-sampledDamage);
+      }
+      else
+      {
+        ApplyHealing(-sampledDamage);
+      }
     }
 
-    public void ApplyHealing()
+    public void ApplyHealing(float healAmount)
     {
+      onHeal?.Invoke();
+      ChangeHealth(healAmount);
     }
 
     private void Die()
     {
       IsDied = true;
+      onDeath?.Invoke();
     }
 
     private void ChangeHealth(float changeAmount)
     {
       var resources = CurrentStats.CharacterResources;
 
-      var baseHealthAmount = baseStats.CharacterResources.Find(i => i.ResourceType == CharacterResourceType.HEALTH).Amount;
+      var baseHealthAmount =
+        baseStats.CharacterResources.Find(i => i.ResourceType == CharacterResourceType.HEALTH).Amount;
 
       var currentHealthIndex = resources.FindIndex(i => i.ResourceType == CharacterResourceType.HEALTH);
       var currentHealthAmount = resources[currentHealthIndex].Amount;
